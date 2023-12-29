2fbe085abce508b37fbcebe3e4a82e2b8f9942e3
yanaivantsova <yanaivantsova@yandex.by>
2023-12-29 20:05:22 +0100

    feat: move the project to framework Vue

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-This project was completed at the intensive “Food Delivery Service” in HTML/CSS/JS/Vue from Glo Academy.
-Design in Figma: https://www.figma.com/file/jvdXKoLWJcJOX3nwiSyfCo/Delivery-Food-(Copy)?node-id=230%3A22216&mode=dev
+# delivery-vue
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/ProductView.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="products">
+    <div class="container">
+      <div class="products-wrapper" id="products-container">
+        <div
+          class="products-card"
+          v-for="product in productsArray"
+          :key="product.id"
+        >
+          <div class="products-card__image">
+            <img :src="getUrl(product.image)" alt="goods1-image" />
+          </div>
+          <div class="products-card__description">
+            <div class="products-card__description-row">
+              <h5 class="products-card__description--name">
+                {{ product.name }}
+              </h5>
+            </div>
+            <div class="products-card__description-row">
+              <p class="products-card__description--text">
+                {{ product.description }}
+              </p>
+            </div>
+            <div class="products-card__description-row">
+              <div class="products-card__description--controls">
+                <button class="btn btn-primary">
+                  В корзину<img
+                    src="../assets/images/items/shopping-cart-white.svg"
+                    alt="cart-icon"
+                  />
+                </button>
+                <span class="products-card__description-controls--price"
+                  >{{ product.price }} ₽</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { productsArray } from "../constants/products";
+
+const getUrl = (name) => {
+  console.log("hello");
+  console.log(name);
+  return new URL(`../assets/images/goods/${name}`, import.meta.url);
+};
+</script>
